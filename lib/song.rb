1b0e431d171489f5032c1ed1229250bac7621eeb
Luke Chen <luke.chen.2010@gmail.com>
2020-05-06 19:37:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,10 @@ class Song
   @@count = 0
   @@genres = []
   
-  def initialize
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @count += 1
   end
   
